d02147682c3c8e395778848f48837a5f657b0eec
shubhT25 <shubhamtayade11235@gmail.com>
2024-09-27 01:28:07 +0530

    Bean Lifecycle - Explicit Intialization

--- SpringCore5/src/com/spring/beans/HelloBean.java
+++ SpringCore5/src/com/spring/beans/HelloBean.java
@@ -1,6 +1,12 @@
 package com.spring.beans;
 
-public class HelloBean {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class HelloBean implements InitializingBean, DisposableBean {
     private String name, message;
 
     public String getName() {
@@ -8,14 +14,17 @@ public class HelloBean {
     }
 
     public void setName(String name) {
+        System.out.println("In setName function");
         this.name = name;
     }
 
     public String getMessage() {
+        System.out.println("In getName function");
         return message;
     }
 
     public void setMessage(String message) {
+        System.out.println("In setMessage function");
         this.message = message;
     }
 
@@ -28,10 +37,10 @@ public class HelloBean {
     public void init() {
         System.out.println("Intialize method");
         name = "Kaustubh";
-        message = "Good Morning!";
+        message = "Good Afternoon!";
     }
-    public void destroy() {
-        System.out.println("Delete method");
+    public void destroyCustom() {
+        System.out.println("Custom delete method");
         name = "";
         message = "";
     }
@@ -41,6 +50,28 @@ public class HelloBean {
     public static HelloBean getInstance(){
         System.out.println("HelloBean Static Factory Method");
         return new HelloBean();
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        name = "Arjun";
+        message = "Good Evening!";
+        System.out.println("In afterPropertieSet");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("In destroy");
+    }
+
+    @PostConstruct
+    public void intialize(){
+        System.out.println("In PostContruct Init");
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        System.out.println("In PreDestroy destroy");
     }
 }
 
